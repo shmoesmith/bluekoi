da3065c7dccd7b031ff6594c84db5fef1dd106ba
Alan Vincent <alan.d.vincent@gmail.com>
2017-07-05 20:22:54 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-
-  get 'reviews/show'
-
-  get 'reviews/update'
-
-  get 'reviews/destroy'
 
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
-    #API ROUTES SHOULD GO HERE
+    resources :reviews do
+      end
   end
 
   #Do not place any routes below this one
