40e10ecf17cd8f00b2b4cbb348f054a4f2353df6
Alan Vincent <alan.d.vincent@gmail.com>
2017-07-21 23:43:31 -0600

    added local background image and imported font

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { Header, Segment } from 'semantic-ui-react';
 import styled from 'styled-components';
+import homepageHeaderBackground from '../assets/homepageHeaderBackground.jpg'
+import '../styles/fonts.css'
 
 const headerBackground = styled.div`
-  background: url('https://pixabay.com/get/eb3db40a20f31c22d9584518a33219c8b66ae3d019b1144393f4c97e/wellness-285587_1920.jpg')
-
+  background: url('${homepageHeaderBackground}') no-repeat center center fixed !important;
+  height: 500px;
+  background-size: cover !important;
 `;
+
+const topHeader = styled.h1`
+font-family: 'Kaushan Script', cursive !important;
+color: 'white' !important;
+`;
+
 class Home extends Component {
 
   // background: https://pixabay.com/get/eb3db40a20f31c22d9584518a33219c8b66ae3d019b1144393f4c97e/wellness-285587_1920.jpg
@@ -13,7 +22,7 @@ class Home extends Component {
   render() {
     return(
       <Segment as={headerBackground}>
-      <Header as='h1' textAlign='center'>Welcome To Blue Koi</Header>
+      <Header as={topHeader} textAlign='center'>Blue Koi Massage</Header>
       </Segment>
     );
   }
